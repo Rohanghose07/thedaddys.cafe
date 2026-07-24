5e0abac04494d908b9bc6bc6cd6401b06421d43b
Rohan Ghose <70902860+Rohanghose07@users.noreply.github.com>
2026-07-24 21:51:40 +0530

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://mcijsobzvsrjpvcpyxuy.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_xw-5aFmYgABBT4QIKTTFzw_AUJRLJKC";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY
-);
